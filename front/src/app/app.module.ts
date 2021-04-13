6899e3f985f5482977be6271750ba3a4325f609b
Ahmed Baig <ahmedbaig54@yahoo.com>
2021-04-13 08:52:33 +0500

    Component create and forms

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/items/list/list.component';
+import { DetailComponent } from './components/items/detail/detail.component';
+import { BotComponent } from './components/bot/bot.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    BotComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
