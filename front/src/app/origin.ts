ef4cbbab7d7373286556c88f40522fff46e6c401
Ahmed Baig <ahmedbaig54@yahoo.com>
2021-04-14 05:03:07 +0500

    Origin updates

--- front/src/app/origin.ts
+++ front/src/app/origin.ts
@@ -1,9 +1,11 @@
 export function getOrigin () : String{
-    let origin = "http://localhost:4040"
+    // let origin = "http://localhost:4040"
+    let origin = "https://scopic-auction.herokuapp.com"
     return origin;
 }
 
 export function getDevOrigin () : String{
-    let origin = "http://localhost:4040"
+    // let origin = "http://localhost:4040"
+    let origin = "https://scopic-auction.herokuapp.com"
     return origin;
 }
